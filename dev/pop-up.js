42d41da9bb8438bd3780b7d0e4b9ce6ed6377027
Matt LaGrandeur <mattl@microsoft.com>
2021-10-13 16:34:37 -0700

    Controls :passport_control:

--- dev/pop-up.js
+++ dev/pop-up.js
@@ -1,8 +1,20 @@
 import { getPeopleData } from './main-page.js';
 
+let _settings = {
+	mask: 'circle',
+	size: 800,
+	includeManagementChain: false
+};
+
+let peopleData = {
+	selected: {},
+	above: [],
+	below: []
+};
+
 // What to do every time the popup is opened
 document.addEventListener('DOMContentLoaded', function() {
-	document.getElementById('bodyContent').innerHTML = '<i style="grid-column: span 3;">Getting people pictures...</i>';
+	document.getElementById('infoContent').innerHTML = '<span><i>Getting people pictures...</i></span>';
 	
 	// So, this tabs.executeScript takes a code string :-\
 	// 'getPeopleData' is defined below, then use fn.toString and call it in an IIFE
@@ -16,93 +28,142 @@ document.addEventListener('DOMContentLoaded', function() {
 	});
 });
 
+
 chrome.runtime.onMessage.addListener(function(message) {
-	console.log(`POPUP Got Message`);
-	console.log(message);	
-	populatePeoplePicturesList(message);
+	console.log(`POPUP.listener - got message`);
+	console.log(message);
+	peopleData = JSON.parse(message);
+	console.log(peopleData);
+
+	redraw();
 });
 
+function redraw() {
+	console.log(`redraw - START`);
 
-function populatePeoplePicturesList(message) {
-	console.log(`populatePeoplePicturesList - START`);
-	let people = JSON.parse(message);
+	redrawInfo();
+	redrawControls();
+	redrawPeoplePicturesList();
+
+	console.log(`redraw - END`);
+}
+
+
+function redrawPeoplePicturesList() {
+	console.log(`redrawPeoplePicturesList - START`);
 	
-	let bodyContent = `
-		<div class="headerRow">
-			<span id="topTitle">Found ${people.below.length} people</span>
-			<button id="openInfo"><span>
-				<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="14px"
-					height="14px" viewBox="0 0 14 14" style="enable-background:new 0 0 14 14;" xml:space="preserve">
-				<path d="M7,0C3.1,0,0,3.1,0,7c0,3.9,3.1,7,7,7s7-3.1,7-7C14,3.1,10.9,0,7,0z M8,12H6V6h2V12z M7,4.5c-0.8,0-1.3-0.6-1.3-1.3
-					S6.4,2,7,2c0.6,0,1.3,0.6,1.3,1.3S7.8,4.5,7,4.5z"/>
-				</svg>
-			</span></button>
-		</div>
-	`;
+	let listContent = `
+	<span>
+		<h2>People Pictures</h2>
+		<i>found ${peopleData.below.length +1} people</i>
+	</span>`;
+	
+	// listContent += `<span>${JSON.stringify(_settings)}</span>`;
+	
+	listContent += makeOnePersonRow(peopleData.selected);
 
-	bodyContent += makeOnePersonRow(people.selected);
-
-	//test
-	people = people.below;
-	console.log(people);
-
-	if(people.length) {
-		people.forEach(person => {
+	if(peopleData.below.length) {
+		peopleData.below.forEach(person => {
 			// Add the picture as a row in the main content
-			bodyContent += makeOnePersonRow(person);
+			listContent += makeOnePersonRow(person);
 		});
 
-		bodyContent += '<i style="grid-column: span 3;">End of people list</i>';	
-		document.getElementById('bodyContent').innerHTML = bodyContent;
+		listContent += '<span><i>End of people list</i></span><br>';
+		document.getElementById('listContent').innerHTML = listContent;
 
 	} else {
-		document.getElementById('bodyContent').innerHTML = '<i style="grid-column: span 3;">No photos found</i>';
+		document.getElementById('listContent').innerHTML = '<span><i>No photos found</i></span><br>';
 	}
-
-	document.getElementById('openInfo').onclick = () => document.getElementById('info').style.display = 'block';
-	document.getElementById('closeInfo').onclick = () => document.getElementById('info').style.display = 'none';
 	
-	console.log(`populatePeoplePicturesList - END`);
+	console.log(`redrawPeoplePicturesList - END`);
 }
 
 function makeOnePersonRow(person) {
-	// console.log(`\n makeOnePersonRow`);
-	// console.log(iconSVG);
-
+	// console.log(`\n makeOnePersonRow - START`);
 	let row = `
 		<div class="rowWrapper">
-			<img src="${person.imgData}" style="grid-column: 1; height:32px;"/>
-			<div style="grid-column: span 2;"><b>${person.fullName}</b> (${person.alias})</div>
+			<img src="${person.imgData}" alt="Photo picture of ${person.fullName}" />
+			<div><b>${person.fullName}</b> (${person.alias})</div>
 		</div>
 	`;
 
-	// let findID = document.createElement('div');
-	// findID.innerHTML = iconSVG;
-	// let elemID = findID.getElementsByTagName('svg') || 'name';
-	// if(elemID[0] && elemID[0].id) elemID = elemID[0].id;
-
-	// let name = nameMap[elemID];
-	// if(!name) name = elemID;
-
-	// idSuffix++;
-	// ids.push(idSuffix);
-
-	// let con = `
-	// 	<div class="rowWrapper" data-search-name="${name}">
-	// 		<div style="grid-column: 1;" class="iconPreview" id="icon${idSuffix}" title="SVG file preview">${iconSVG}</div>
-	// 		<div style="grid-column: 2;" class="iconName">
-	// 			<input type="text"  id="name${idSuffix}" value="${name}" title="Rename the SVG file"></input>
-	// 		</div>
-	// 		<button style="grid-column: 3;" class="downloadButton" id="button${idSuffix}" title="Download the SVG file">
-	// 			<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve"
-	// 				x="0px" y="0px" width="14px" height="16px" viewBox="0 0 14 16" style="enable-background:new 0 0 14 16;">
-	// 				<polygon points="12,7 8.5,10.5 8.5,0 5.5,0 5.5,10.5 2,7 0,9 7,16 14,9 "/>
-	// 			</svg>
-	// 		</button>
-	// 	</div>
-	// `;
-
 	return row;
+}
+
+function redrawControls() {
+	let controlsContent = `<h2>Options</h2>`;
+
+	controlsContent += `
+		<label for="maskInput">Shape mask</label>
+		<select onChange="updateSettings();" id="maskInput">
+			<option ${_settings.mask === 'square' ? 'selected="selected" ' : ''}value="square">Square</option>
+			<option ${_settings.mask === 'circle' ? 'selected="selected" ' : ''}value="circle">Circle</option>
+			<option ${_settings.mask === 'none' ? 'selected="selected" ' : ''}value="none">None</option>
+		</select>
+	`;
+
+	controlsContent += `
+		<label>Resize (px)</label>
+		<input onChange="updateSettings();" type="number" id="sizeInput" value="${_settings.size}">
+	`;
+
+	controlsContent += `
+		<label for="includeManagementChainInput">Include management chain</label>
+		<input onChange="updateSettings();" type="checkbox" ${_settings.includeManagementChain? 'checked ' : ''}id="includeManagementChainInput">
+	`;
+
+	document.getElementById('controlsContent').innerHTML = controlsContent;
+	
+	document.getElementById('maskInput').onchange = updateSettings;
+	document.getElementById('sizeInput').onchange = updateSettings;
+	document.getElementById('includeManagementChainInput').onchange = updateSettings;
+}
+
+function updateSettings() {
+	let maskSetting = document.getElementById('maskInput').value;
+	let sizeSetting = parseInt(document.getElementById('sizeInput').value);
+	let includeManagementChainSetting = document.getElementById('includeManagementChainInput').checked;
+
+	console.log(`updateSettings - setting to ${maskSetting} ${sizeSetting} ${includeManagementChainSetting}`);
+	_settings = {
+		mask: maskSetting,
+		size: sizeSetting,
+		includeManagementChain: includeManagementChainSetting
+	};
+
+	redrawPeoplePicturesList();
+}
+
+function redrawInfo() {
+	
+	document.getElementById('infoContent').innerHTML = `
+		<div class="headerRow">
+			<button id="downloadButton">
+				<span>
+					<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="14px"
+						height="14px" viewBox="0 0 14 14" style="enable-background:new 0 0 14 14;" xml:space="preserve">
+						<polygon points="6,0 8,0 8,12 13,7 14,8 7,14 0,8 1,7 6,12"/>
+					</svg>&nbsp;Download&nbsp;people&nbsp;pictures&nbsp;
+				</span>
+			</button>
+			<button id="openInfo">
+				<span>
+					<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" 
+						x="0px" y="0px" width="14px" height="14px" 
+						viewBox="0 0 14 14" style="enable-background:new 0 0 14 14;" xml:space="preserve">
+					<path d="
+						M7,0C3.1,0,0,3.1,0,7c0,3.9,3.1,7,7,7s7-3.1,7-7C14,3.1,10.9,0,7,0z
+						M8,12H6V6h2V12z
+						M7,4.5c-0.8,0-1.3-0.6-1.3-1.3 S6.4,2,7,2c0.6,0,1.3,0.6,1.3,1.3S7.8,4.5,7,4.5z
+					"/>
+					</svg>
+				</span>
+			</button>
+		</div>
+	`;
+
+	document.getElementById('openInfo').onclick = () => document.getElementById('infoDialog').style.display = 'block';
+	document.getElementById('closeInfo').onclick = () => document.getElementById('infoDialog').style.display = 'none';
 }
 
 function downloadFile(name = 'icon', fContent = '') {
